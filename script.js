4f49b9514528e81046bf911bc0c7fdeda14ebd7b
ajayy222 <jangid562ajay@gmail.com>
2025-06-23 11:34:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+    const navbar = document.getElementById('navbar');
+    const navLinks = document.getElementById('nav-links');
+
+    window.addEventListener('scroll', () => {
+      navbar.classList.toggle('scrolled', window.scrollY > 50);
+    });
+
+    function toggleMenu() {
+      navLinks.classList.toggle('active');
+    }
